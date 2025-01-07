0689f7bf374c8bb9a22c8218752d57c64f7ba64e
Bartosz Budzynowski <bartoszbud@gmail.com>
2025-01-07 21:11:43 +0100

    Update main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,22 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "${var.environment}-vpc"
+    Description = "${var.description}"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  for_each                = var.subnets
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = each.value["cidr"]
+  availability_zone       = each.value["availability_zone"]
+  map_public_ip_on_launch = each.value["map_public_ip_on_launch"]
+  
+  tags = {
+    Name = each.key
+  }   
+}
